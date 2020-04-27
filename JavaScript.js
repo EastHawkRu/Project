671c7d7c7ffc6c890f1490d324f401d878c670d5
Daniil <ITRodionov@yandex.ru>
2020-04-27 07:12:44 +0300

    first commit

---
+++ JavaScript.js
@@ -0,0 +1,36 @@
+ 'use strict';
+
+// Классификации типов данных// 
+var number = 5;
+var string = "Привет";
+var sym = Symbol();
+var boolean = true;
+null; // Ссылка на несуществующую вещь или ошибка зпроса консоли данных нет в коде//
+undefined; //когда переменная сохранена без значения//
+var obj = {}; //Группа данных. Все данные внутри называеются свойствами объекта//
+
+//Примеры://
+
+let persone = {
+    name: alex,
+    age: 24,
+    isMarried: false
+} // Это объект//
+
+let arr = ["Eboshi.png", "Flowers.png", "Heroes.png"] // Это массив, в массиве данные идут берут счёт с нуля.//
+console.log (arr[0]);
+
+alert("Hello World!") //Модальное окно//
+confirm ("Are you here?"); // Модальное окно с выбором подтверждения//
+prompt("Are you 18 years old?") //Команда для ввода ответа пользователем//
+console.log (Typeof(answer)); //Команда тайпоф позволяет узнать тип данных которые ввёл пользователь//
+
+
+let incr = 10,
+    decr = 10;
+
+incr ++;
+decr --;
+
+console.log (incr);
+console.log (decr);
---
+++ Script.js
@@ -0,0 +1,4 @@
+'Use strict'
+
+confirm ("Hello World!!!");
+
